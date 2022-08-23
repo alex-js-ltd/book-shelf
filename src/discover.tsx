5a986cf647bd73cfc537dba95a1b8ae4aa4d7c56
Alex Gilbert <alex.js.ltd@gmail.com>
2022-08-23 12:13:24 +0200

    update

--- src/discover.tsx
+++ src/discover.tsx
@@ -1,9 +1,34 @@
-import React, { FC } from 'react';
+/** @jsx jsx */
+import { jsx } from '@emotion/react';
+
+import React, { FC, useEffect } from 'react';
 import Tooltip from '@reach/tooltip';
 import { FaSearch, FaTimes } from 'react-icons/fa';
 import { Input, BookListUL, Spinner } from './comps/lib';
+import { BookRow } from './comps/book-row';
+import { getBooks } from './firebase/get-books';
+import * as colors from './styles/colors';
+import { useAsync } from './utils/hooks';
 
 const DiscoverBooksScreen: FC = () => {
+  const { data, error, run, isLoading, isError, isSuccess } = useAsync();
+
+  useEffect(() => {
+    run(getBooks());
+  }, [run]);
+
+  useEffect(() => {
+    console.log('data', data);
+  }, [data]);
+
+  useEffect(() => {}, []);
+
+  useEffect(() => {
+    console.log('isLoading', isLoading);
+    console.log('isError', isError);
+    console.log('isSuccess', isSuccess);
+  }, [isLoading, isError, isSuccess]);
+
   return <div>DiscoverBooksScreen</div>;
 };
 
---
+++ src/firebase/get-books.ts
@@ -0,0 +1,18 @@
+import { collection, query, getDocs } from 'firebase/firestore';
+import { idText } from 'typescript';
+import { db } from '../firebase';
+
+const getBooks = async (): Promise<any[]> => {
+  const collectionRef = collection(db, 'books');
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+  return querySnapshot.docs.map((docSnapshot) => {
+    return {
+      id: docSnapshot.id,
+      ...docSnapshot.data(),
+    };
+  });
+};
+
+export { getBooks };
