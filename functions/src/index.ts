17ad1bd38bdcb8d577f8312b303784c7d5235988
Alex Gilbert <alex.js.ltd@gmail.com>
2022-08-25 17:12:13 +0200

    update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,33 @@
-//  import * as functions from "firebase-functions";
+// @ts-nocheck
+const functions = require('firebase-functions');
+const algoliasearch = require('algoliasearch');
+
+const APP_ID = functions.config().algolia.app;
+const ADMIN_KEY = functions.config().algolia.key;
+
+const client = algoliasearch(APP_ID, ADMIN_KEY);
+const index = client.initIndex('books');
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+exports.addToIndex = functions.firestore
+  .document('books/{bookId}')
+  .onCreate((snapshot) => {
+    const data = snapshot.data();
+    const objectID = snapshot.id;
+
+    return index.saveObject({ ...data, objectID });
+  });
+
+exports.updateIndex = functions.firestore
+  .document('books/{bookId}')
+  .onUpdate((change) => {
+    const newData = change.after.data();
+    const objectID = change.after.id;
+    return index.saveObject({ ...newData, objectID });
+  });
+
+exports.deleteFromIndex = functions.firestore
+  .document('books/{bookId}')
+  .onDelete((snapshot) => index.deleteObject(snapshot.id));
