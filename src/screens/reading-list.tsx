39b0eff2def234b4575c795f23134edad8669124
Alex Gilbert <alex.js.ltd@gmail.com>
2022-08-31 16:20:33 +0100

    update

--- src/screens/reading-list.tsx
+++ src/screens/reading-list.tsx
@@ -2,19 +2,17 @@ import * as React from 'react';
 import { Link } from 'comps/lib';
 import { ListItemList } from 'comps/list-item-list';
 
-const ReadingListScreen = () => {
-  return (
-    <ListItemList
-      filterListItems={(li: any) => !li.finishDate}
-      noListItems={
-        <p>
-          Hey there! Welcome to your bookshelf reading list. Get started by
-          heading over to <Link to='/discover'>the Discover page</Link> to add
-          books to your list.
-        </p>
-      }
-    />
-  );
-};
+const ReadingListScreen = () => (
+  <ListItemList
+    filterListItems={(li: any) => !li.finishDate}
+    noListItems={
+      <p>
+        Hey there! Welcome to your bookshelf reading list. Get started by
+        heading over to <Link to='/discover'>the Discover page</Link> to add
+        books to your list.
+      </p>
+    }
+  />
+);
 
 export { ReadingListScreen };
