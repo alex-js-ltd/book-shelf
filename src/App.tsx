ed373d2261042d1b88cb38d8c03602f521745f9e
Alex Gilbert <alex.js.ltd@gmail.com>
2022-09-02 21:01:26 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import * as React from 'react';
 import { useAuth } from './context/auth-context';
-import { FullPageSpinner } from 'comps/lib';
 
-const AuthenticatedApp = React.lazy(() => import('./authenticated-app'));
-const UnauthenticatedApp = React.lazy(() => import('./unauthenticated-app'));
+import UnauthenticatedApp from './unauthenticated-app';
+import AuthenticatedApp from './authenticated-app';
 
-const App = () => {
+const App: React.FC = () => {
   const { user } = useAuth();
-  return (
-    <React.Suspense fallback={<FullPageSpinner />}>
-      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
-    </React.Suspense>
-  );
+  return user ? <AuthenticatedApp /> : <UnauthenticatedApp />;
 };
 
 export default App;
