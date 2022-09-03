682a76e5aea36abc4f842f2d7eaf53e9fd6e842b
Alex Gilbert <alex.js.ltd@gmail.com>
2022-09-03 01:20:10 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import { useAuth } from './context/auth-context';
+import { FullPageSpinner } from 'comps/lib';
 
 const AuthenticatedApp = React.lazy(() => import('./authenticated-app'));
 const UnauthenticatedApp = React.lazy(() => import('./unauthenticated-app'));
 
 const App: React.FC = () => {
   const { user } = useAuth();
-  return user ? <AuthenticatedApp /> : <UnauthenticatedApp />;
+  return (
+    <React.Suspense fallback={<FullPageSpinner />}>
+      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+    </React.Suspense>
+  );
 };
 
 export default App;
--- src/__tests__/book-screen.js
+++ src/__tests__/book-screen.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import { AppProviders } from 'context';
 import App from 'App';
 
 test('renders all the book information', async () => {
   render(<App />, { wrapper: AppProviders });
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i));
   screen.debug();
 });
