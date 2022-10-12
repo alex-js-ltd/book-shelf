d8f40c01f3ac05b5101387f16ba94297ad6dccdd
Alex Gilbert <alex.js.ltd@gmail.com>
2022-10-12 17:06:15 +0200

    update

--- src/screens/finished.tsx
+++ src/screens/finished.tsx
@@ -2,14 +2,15 @@ import * as React from 'react';
 import { Link } from 'comps/lib';
 import { ListItemList } from 'comps/list-item-list';
 
-const FinishedScreen: React.FC = () => (
+const FinishedScreen = () => (
   <ListItemList
     filterListItems={(li: any) => Boolean(li.finishDate)}
     noListItems={
       <p>
         Hey there! This is where books will go when you've finished reading
         them. Get started by heading over to
-        <Link to='/discover'>the Discover page</Link> to add books to your list.
+        <Link to='/discover'> the Discover page</Link> to add books to your
+        list.
       </p>
     }
   />
