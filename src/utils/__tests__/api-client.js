b15e1eaa0142ec46d68f8bc223fbf3e489c6b7ca
Alex Gilbert <alex.js.ltd@gmail.com>
2022-10-17 18:24:52 +0200

    update

--- src/utils/__tests__/api-client.js
+++ src/utils/__tests__/api-client.js
@@ -2,8 +2,6 @@ import { client } from '../api-client'
 import { server } from 'test/server/test-server'
 import { rest } from 'msw'
 
-const apiURL = process.env.REACT_APP_API_URL
-
 beforeAll(() => server.listen())
 afterAll(() => server.close())
 
@@ -19,4 +17,22 @@ test('calls fetch at the endpoint with the arguments for GET requests', async ()
 	const result = await client(endpoint, { method: 'GET' })
 
 	expect(result).toEqual(mockResult)
+})
+
+test('adds auth token when a token is provided', async () => {
+	const token = 'FAKE_TOKEN'
+
+	let request
+	const endpoint = 'test-endpoint'
+	const mockResult = { mockValue: 'VALUE' }
+	server.use(
+		rest.get(`${endpoint}`, async (req, res, ctx) => {
+			request = req
+			return res(ctx.json(mockResult))
+		}),
+	)
+
+	await client(endpoint, { method: 'GET', token })
+
+	expect(request.headers.get('Authorization')).toBe(`Bearer ${token}`)
 })
