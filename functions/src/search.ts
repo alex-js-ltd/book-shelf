f2e2f05b5826649c08cc96715f4bc7a977c647ab
Alex Gilbert <alex.js.ltd@gmail.com>
2022-12-02 10:30:39 +0100

    update

---
+++ functions/src/search.ts
@@ -0,0 +1,35 @@
+import * as functions from 'firebase-functions'
+import algoliasearch from 'algoliasearch'
+
+const APP_ID = functions.config().algolia.app
+const ADMIN_KEY = functions.config().algolia.key
+
+const client = algoliasearch(APP_ID, ADMIN_KEY)
+const index = client.initIndex('books')
+
+const addToIndex = functions.firestore
+	.document('books/{bookId}')
+	.onCreate(snapshot => {
+		const data = snapshot.data()
+		const objectID = snapshot.id
+
+		return index.saveObject({ ...data, objectID })
+	})
+
+const updateIndex = functions.firestore
+	.document('books/{bookId}')
+	.onUpdate(change => {
+		const newData = change.after.data()
+		const objectID = change.after.id
+		return index.saveObject({ ...newData, objectID })
+	})
+
+const deleteFromIndex = functions.firestore
+	.document('books/{bookId}')
+	.onDelete(snapshot => index.deleteObject(snapshot.id))
+
+module.exports = {
+	addToIndex,
+	updateIndex,
+	deleteFromIndex,
+}
---
+++ functions/src/user.ts
@@ -0,0 +1,24 @@
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+
+admin.initializeApp()
+
+const db = admin.firestore()
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+
+const createUserRecod = functions.auth.user().onCreate((user, context) => {
+	const userRef = db.doc(`users/${user.uid}`)
+
+	return userRef.set({
+		email: user.email,
+		uid: user.uid,
+		createdAt: context.timestamp,
+		readingList: [],
+	})
+})
+
+module.exports = {
+	createUserRecod,
+}
