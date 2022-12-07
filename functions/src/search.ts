9274abb2d719123721537988b1024239b3d292f8
Alex Gilbert <alex.js.ltd@gmail.com>
2022-12-07 21:58:50 +0100

    update

--- functions/src/search.ts
+++ functions/src/search.ts
@@ -1,29 +1,29 @@
-import * as functions from 'firebase-functions'
+import { config, firestore } from 'firebase-functions'
 import algoliasearch from 'algoliasearch'
 
-const APP_ID = functions.config().algolia.app
-const ADMIN_KEY = functions.config().algolia.key
+const APP_ID = config().algolia.app
+const ADMIN_KEY = config().algolia.key
 
 const client = algoliasearch(APP_ID, ADMIN_KEY)
 const index = client.initIndex('books')
 
-export const addToIndex = functions.firestore
-	.document('books/{bookId}')
-	.onCreate(snapshot => {
-		const data = snapshot.data()
-		const objectID = snapshot.id
+export const addToIndex = firestore
+  .document('books/{bookId}')
+  .onCreate(snapshot => {
+    const data = snapshot.data()
+    const objectID = snapshot.id
 
-		return index.saveObject({ ...data, objectID })
-	})
+    return index.saveObject({ ...data, objectID })
+  })
 
-export const updateIndex = functions.firestore
-	.document('books/{bookId}')
-	.onUpdate(change => {
-		const newData = change.after.data()
-		const objectID = change.after.id
-		return index.saveObject({ ...newData, objectID })
-	})
+export const updateIndex = firestore
+  .document('books/{bookId}')
+  .onUpdate(change => {
+    const newData = change.after.data()
+    const objectID = change.after.id
+    return index.saveObject({ ...newData, objectID })
+  })
 
-export const deleteFromIndex = functions.firestore
-	.document('books/{bookId}')
-	.onDelete(snapshot => index.deleteObject(snapshot.id))
+export const deleteFromIndex = firestore
+  .document('books/{bookId}')
+  .onDelete(snapshot => index.deleteObject(snapshot.id))
--- functions/src/user.ts
+++ functions/src/user.ts
@@ -1,23 +1,18 @@
-import * as functions from 'firebase-functions'
-import { initializeApp } from 'firebase-admin/app'
+import { auth } from 'firebase-functions'
 import { getFirestore } from 'firebase-admin/firestore'
-
-initializeApp()
 
 const db = getFirestore()
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
-export const createUserRecord = functions.auth
-	.user()
-	.onCreate((user, context) => {
-		const userRef = db.doc(`users/${user.uid}`)
+export const createUserRecord = auth.user().onCreate((user, context) => {
+  const userRef = db.doc(`users/${user.uid}`)
 
-		return userRef.set({
-			email: user.email,
-			uid: user.uid,
-			createdAt: context.timestamp,
-			readingList: [],
-		})
-	})
+  return userRef.set({
+    email: user.email,
+    uid: user.uid,
+    createdAt: context.timestamp,
+    readingList: [],
+  })
+})
