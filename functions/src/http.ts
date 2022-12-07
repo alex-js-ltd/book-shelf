ae03f075123dae2469263b35d1a5a242758f6e6c
Alex Gilbert <alex.js.ltd@gmail.com>
2022-12-07 17:34:32 +0100

    update

--- functions/src/http.ts
+++ functions/src/http.ts
@@ -8,16 +8,26 @@ import 'express-async-errors'
 import logger from 'loglevel'
 
 import { getRoutes } from './routes'
+import { validateFirebaseIdToken } from './validate-firebase-tokenId'
 
 initializeApp()
 
 const app = express()
 app.use(cors({ origin: true }))
+app.use(validateFirebaseIdToken)
 app.use('/', getRoutes())
 app.use(errorMiddleware)
 
+function isError(error: unknown) {
+  let message = 'Unknown Error'
+  if (error instanceof Error) {
+    message = error.message
+    return { message, stack: error.stack }
+  } else return { message }
+}
+
 function errorMiddleware(
-  error: any,
+  error: unknown,
   _req: Request,
   res: Response,
   next: NextFunction,
@@ -25,14 +35,15 @@ function errorMiddleware(
   if (res.headersSent) {
     next(error)
   } else {
+    const { message, stack } = isError(error)
+
     logger.error(error)
+
     res.status(500)
     res.json({
-      message: error.message,
+      message: message,
       // we only add a `stack` property in non-production environments
-      ...(process.env.NODE_ENV === 'production'
-        ? null
-        : { stack: error.stack }),
+      ...(process.env.NODE_ENV === 'production' ? null : { stack }),
     })
   }
 }
--- src/utils/api-client.ts
+++ src/utils/api-client.ts
@@ -1,52 +1,52 @@
 import { queryClient } from 'context'
 import * as auth from 'auth-provider'
 import { Config } from 'client-types'
-import { Book, ListItem } from '../../types'
+import { ListItem } from '../../types'
 const apiURL = process.env.REACT_APP_API_URL
 
 async function client(endpoint: string, { method, data, token }: Config) {
-	const config = {
-		method: method,
-		body: data ? JSON.stringify(data) : undefined,
-		headers: {
-			Authorization: `Bearer ${token}`,
-			'Content-Type': 'application/json',
-		},
-	}
+  const config = {
+    method: method,
+    body: data ? JSON.stringify(data) : undefined,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+  }
 
-	return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
-		if (response.status === 401) {
-			queryClient.clear()
-			await auth.logout()
-			// refresh the page for them
-			window.location.assign(window.location.href)
-			return Promise.reject({ message: 'Please re-authenticate.' })
-		}
-		const data = await response.json()
+  return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
+    if (response.status === 401) {
+      queryClient.clear()
+      await auth.logout()
+      // refresh the page for them
+      window.location.assign(window.location.href)
+      return Promise.reject({ message: 'Please re-authenticate.' })
+    }
+    const data = await response.json()
 
-		if (response.ok) {
-			return data
-		} else {
-			const error = new Error(data?.error?.message)
-			return Promise.reject(error)
-		}
-	})
+    if (response.ok) {
+      return data
+    } else {
+      const error = new Error(data?.error?.message)
+      return Promise.reject(error)
+    }
+  })
 }
 
 function read(endpoint: string, token: string) {
-	return client(endpoint, { method: 'GET', token })
+  return client(endpoint, { method: 'GET', token })
 }
 
 function create(endpoint: string, data: ListItem, token: string) {
-	return client(endpoint, { method: 'POST', data, token })
+  return client(endpoint, { method: 'POST', data, token })
 }
 
 function update(endpoint: string, data: ListItem, token: string) {
-	return client(endpoint, { method: 'PUT', data, token })
+  return client(endpoint, { method: 'PUT', data, token })
 }
 
 function remove(endpoint: string, data: ListItem, token: string) {
-	return client(endpoint, { method: 'DELETE', data, token })
+  return client(endpoint, { method: 'DELETE', data, token })
 }
 
 export { read, create, update, remove }
