40d17e8545f08fb29af9cd91b385a6b99896bbc8
Alex Gilbert <alex.js.ltd@gmail.com>
2022-12-07 16:21:40 +0100

    update

--- functions/src/http.ts
+++ functions/src/http.ts
@@ -2,9 +2,10 @@ import { https } from 'firebase-functions'
 import { initializeApp } from 'firebase-admin/app'
 
 // Express
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import cors from 'cors'
 import 'express-async-errors'
+import logger from 'loglevel'
 
 import { getRoutes } from './routes'
 
@@ -13,5 +14,27 @@ initializeApp()
 const app = express()
 app.use(cors({ origin: true }))
 app.use('/', getRoutes())
+app.use(errorMiddleware)
+
+function errorMiddleware(
+  error: any,
+  _req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  if (res.headersSent) {
+    next(error)
+  } else {
+    logger.error(error)
+    res.status(500)
+    res.json({
+      message: error.message,
+      // we only add a `stack` property in non-production environments
+      ...(process.env.NODE_ENV === 'production'
+        ? null
+        : { stack: error.stack }),
+    })
+  }
+}
 
 export const api = https.onRequest(app)
