ca18234a41bde15e059d0be1a952fc8dcfda2509
Alex Gilbert <alex.js.ltd@gmail.com>
2022-12-12 10:45:40 +0100

    update

--- src/comps/list-item-list.tsx
+++ src/comps/list-item-list.tsx
@@ -1,31 +1,30 @@
 import { ReactNode } from 'react'
-
 import { BookListUL } from './lib'
 import { BookRow } from './book-row'
 import { Book } from '../../types'
 
-const ListItemList = ({
-	noListItems,
-	list,
+function ListItemList({
+  noListItems,
+  list,
 }: {
-	noListItems: ReactNode
-	list: Book[]
-}) => {
-	if (!list?.length) {
-		return (
-			<div css={{ marginTop: '1em', fontSize: '1.2em' }}>{noListItems}</div>
-		)
-	}
+  noListItems: ReactNode
+  list: Book[]
+}) {
+  if (!list?.length) {
+    return (
+      <div css={{ marginTop: '1em', fontSize: '1.2em' }}>{noListItems}</div>
+    )
+  }
 
-	return (
-		<BookListUL>
-			{list?.map((listItem: Book) => (
-				<li key={listItem.objectID}>
-					<BookRow book={listItem} />
-				</li>
-			))}
-		</BookListUL>
-	)
+  return (
+    <BookListUL>
+      {list?.map((listItem: Book) => (
+        <li key={listItem.objectID}>
+          <BookRow book={listItem} />
+        </li>
+      ))}
+    </BookListUL>
+  )
 }
 
 export { ListItemList }
