2eddf0597a480831f99a2f1a3a95cfc57b56bc1b
Alex Gilbert <alex.js.ltd@gmail.com>
2022-12-12 13:57:51 +0100

    update

--- src/comps/lib.tsx
+++ src/comps/lib.tsx
@@ -9,171 +9,171 @@ import { Dialog as ReachDialog } from '@reach/dialog'
 import { FaSpinner } from 'react-icons/fa'
 
 const spin = keyframes({
-	'0%': { transform: 'rotate(0deg)' },
-	'100%': { transform: 'rotate(360deg)' },
+  '0%': { transform: 'rotate(0deg)' },
+  '100%': { transform: 'rotate(360deg)' },
 })
 
 const CircleButton = styled.button({
-	borderRadius: '30px',
-	padding: '0',
-	width: '40px',
-	height: '40px',
-	lineHeight: '1',
-	display: 'flex',
-	alignItems: 'center',
-	justifyContent: 'center',
-	background: colors.base,
-	color: colors.text,
-	border: `1px solid ${colors.gray10}`,
-	cursor: 'pointer',
+  borderRadius: '30px',
+  padding: '0',
+  width: '40px',
+  height: '40px',
+  lineHeight: '1',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  background: colors.base,
+  color: colors.text,
+  border: `1px solid ${colors.gray10}`,
+  cursor: 'pointer',
 })
 
 const BookListUL = styled.ul({
-	listStyle: 'none',
-	padding: '0',
-	display: 'grid',
-	gridTemplateRows: 'repeat(auto-fill, minmax(100px, 1fr))',
-	gridGap: '1em',
+  listStyle: 'none',
+  padding: '0',
+  display: 'grid',
+  gridTemplateRows: 'repeat(auto-fill, minmax(100px, 1fr))',
+  gridGap: '1em',
 })
 
 const Spinner = styled(FaSpinner)({
-	animation: `${spin} 1s linear infinite`,
+  animation: `${spin} 1s linear infinite`,
 })
 Spinner.defaultProps = {
-	'aria-label': 'loading',
+  'aria-label': 'loading',
 }
 
 type ButtonVariant = { primary: CSSObject; secondary: CSSObject }
 
 const buttonVariants: ButtonVariant = {
-	primary: {
-		background: colors.indigo,
-		color: colors.base,
-	},
-	secondary: {
-		background: colors.gray,
-		color: colors.text,
-	},
+  primary: {
+    background: colors.indigo,
+    color: colors.base,
+  },
+  secondary: {
+    background: colors.gray,
+    color: colors.text,
+  },
 }
 const Button = styled.button<{ variant: keyof ButtonVariant }>(
-	{
-		padding: '10px 15px',
-		border: '0',
-		lineHeight: '1',
-		borderRadius: '3px',
-	},
-	({ variant = 'secondary' }) => buttonVariants[variant],
+  {
+    padding: '10px 15px',
+    border: '0',
+    lineHeight: '1',
+    borderRadius: '3px',
+  },
+  ({ variant = 'secondary' }) => buttonVariants[variant],
 )
 
 const inputStyles = {
-	border: '1px solid #f1f1f4',
-	background: '#f1f2f7',
-	padding: '8px 12px',
+  border: '1px solid #f1f1f4',
+  background: '#f1f2f7',
+  padding: '8px 12px',
 }
 
 const Input = styled.input({ borderRadius: '3px' }, inputStyles)
 const Textarea = styled.textarea(inputStyles)
 
 const Dialog = styled(ReachDialog)({
-	maxWidth: '450px',
-	borderRadius: '3px',
-	paddingBottom: '3.5em',
-	boxShadow: '0 10px 30px -5px rgba(0, 0, 0, 0.2)',
-	margin: '20vh auto',
-	[mq.small]: {
-		width: '100%',
-		margin: '10vh auto',
-	},
+  maxWidth: '450px',
+  borderRadius: '3px',
+  paddingBottom: '3.5em',
+  boxShadow: '0 10px 30px -5px rgba(0, 0, 0, 0.2)',
+  margin: '20vh auto',
+  [mq.small]: {
+    width: '100%',
+    margin: '10vh auto',
+  },
 })
 
 const FormGroup = styled.div({
-	display: 'flex',
-	flexDirection: 'column',
+  display: 'flex',
+  flexDirection: 'column',
 })
 
 const FullPageSpinner = () => (
-	<div
-		css={{
-			fontSize: '4em',
-			height: '100vh',
-			display: 'flex',
-			flexDirection: 'column',
-			justifyContent: 'center',
-			alignItems: 'center',
-		}}
-	>
-		<Spinner />
-	</div>
+  <div
+    css={{
+      fontSize: '4em',
+      height: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <Spinner />
+  </div>
 )
 
 const Link = styled(RouterLink)({
-	color: colors.indigo,
-	':hover': {
-		color: colors.indigoDarken10,
-		textDecoration: 'underline',
-	},
+  color: colors.indigo,
+  ':hover': {
+    color: colors.indigoDarken10,
+    textDecoration: 'underline',
+  },
 })
 
 type ErrVariant = { stacked: CSSObject; inline: CSSObject }
 
 const errorMessageVariants: ErrVariant = {
-	stacked: { display: 'block' },
-	inline: { display: 'inline-block' },
+  stacked: { display: 'block' },
+  inline: { display: 'inline-block' },
 }
 
 const ErrorMessage = ({
-	error,
-	variant = 'stacked',
-	...props
+  error,
+  variant = 'stacked',
+  ...props
 }: {
-	error: Error | null
-	variant?: keyof ErrVariant
+  error: Error | null
+  variant?: keyof ErrVariant
 }) => (
-	<div
-		role='alert'
-		css={[{ color: colors.danger }, errorMessageVariants[variant]]}
-		{...props}
-	>
-		<span>There was an error: </span>
-		<pre
-			css={[
-				{ whiteSpace: 'break-spaces', margin: '0', marginBottom: -5 },
-				errorMessageVariants[variant],
-			]}
-		>
-			{error?.message}
-		</pre>
-	</div>
+  <div
+    role="alert"
+    css={[{ color: colors.danger }, errorMessageVariants[variant]]}
+    {...props}
+  >
+    <span>There was an error: </span>
+    <pre
+      css={[
+        { whiteSpace: 'break-spaces', margin: '0', marginBottom: -5 },
+        errorMessageVariants[variant],
+      ]}
+    >
+      {error?.message}
+    </pre>
+  </div>
 )
 
 const FullPageErrorFallback = ({ error }: { error: Error }) => (
-	<div
-		role='alert'
-		css={{
-			color: colors.danger,
-			height: '100vh',
-			display: 'flex',
-			flexDirection: 'column',
-			justifyContent: 'center',
-			alignItems: 'center',
-		}}
-	>
-		<p>Uh oh... There's a problem. Try refreshing the app.</p>
-		<pre>{error?.message}</pre>
-	</div>
+  <div
+    role="alert"
+    css={{
+      color: colors.danger,
+      height: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <p>Uh oh... There's a problem. Try refreshing the app.</p>
+    <pre>{error?.message}</pre>
+  </div>
 )
 
 export {
-	FullPageErrorFallback,
-	ErrorMessage,
-	CircleButton,
-	BookListUL,
-	Spinner,
-	Button,
-	Input,
-	Textarea,
-	Dialog,
-	FormGroup,
-	FullPageSpinner,
-	Link,
+  FullPageErrorFallback,
+  ErrorMessage,
+  CircleButton,
+  BookListUL,
+  Spinner,
+  Button,
+  Input,
+  Textarea,
+  Dialog,
+  FormGroup,
+  FullPageSpinner,
+  Link,
 }
--- src/utils/list-items.tsx
+++ src/utils/list-items.tsx
@@ -1,84 +1,84 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import { useAuth } from 'context/auth-context'
 import { useClient } from './use-client'
-import { ListItem } from '../../types'
+import { Book, ListItem } from '../../types'
 
 function useListItems() {
-	const { read } = useClient()
-	const { user } = useAuth()
-	const userId = user?.localId
+  const { read } = useClient()
+  const { user } = useAuth()
+  const userId = user?.localId
 
-	const result = useQuery<ListItem[], Error>({
-		queryKey: ['list-items'],
-		queryFn: () => read(`reading-list/${userId}`),
-	})
+  const result = useQuery<ListItem[], Error>({
+    queryKey: ['list-items'],
+    queryFn: () => read(`reading-list/${userId}`),
+  })
 
-	return result?.data ?? []
+  return result?.data ?? []
 }
 
 function useListItem(listItem: ListItem): ListItem | null {
-	const listItems = useListItems()
+  const listItems = useListItems()
 
-	return (
-		listItems?.find((li: ListItem) => li.objectID === listItem.objectID) ?? null
-	)
+  return (
+    listItems?.find((li: ListItem) => li.objectID === listItem.objectID) ?? null
+  )
 }
 
 function useCreateListItem() {
-	const { create } = useClient()
-	const { user } = useAuth()
-	const userId = user?.localId
+  const { create } = useClient()
+  const { user } = useAuth()
+  const userId = user?.localId
 
-	const queryClient = useQueryClient()
+  const queryClient = useQueryClient()
 
-	return useMutation({
-		mutationFn: (listItem: ListItem) =>
-			create(`reading-list/${userId}`, listItem),
+  return useMutation({
+    mutationFn: (listItem: ListItem) =>
+      create(`reading-list/${userId}`, listItem),
 
-		async onSettled() {
-			await queryClient.invalidateQueries(['list-items'])
-		},
-	})
+    async onSettled() {
+      await queryClient.invalidateQueries(['list-items'])
+    },
+  })
 }
 
 function useUpdateListItem() {
-	const { update } = useClient()
-	const { user } = useAuth()
-	const userId = user?.localId
+  const { update } = useClient()
+  const { user } = useAuth()
+  const userId = user?.localId
 
-	const queryClient = useQueryClient()
+  const queryClient = useQueryClient()
 
-	return useMutation({
-		mutationFn: (listItem: ListItem) =>
-			update(`reading-list/${userId}`, listItem),
+  return useMutation<ListItem, Error, ListItem>({
+    mutationFn: (listItem: ListItem) =>
+      update(`reading-list/${userId}`, listItem),
 
-		async onSettled() {
-			await queryClient.invalidateQueries(['list-items'])
-		},
-	})
+    async onSettled() {
+      await queryClient.invalidateQueries(['list-items'])
+    },
+  })
 }
 
 function useRemoveListItem() {
-	const { remove } = useClient()
-	const { user } = useAuth()
-	const userId = user?.localId
+  const { remove } = useClient()
+  const { user } = useAuth()
+  const userId = user?.localId
 
-	const queryClient = useQueryClient()
+  const queryClient = useQueryClient()
 
-	return useMutation({
-		mutationFn: (listItem: ListItem) =>
-			remove(`reading-list/${userId}`, listItem),
+  return useMutation({
+    mutationFn: (listItem: ListItem) =>
+      remove(`reading-list/${userId}`, listItem),
 
-		async onSettled() {
-			await queryClient.invalidateQueries(['list-items'])
-		},
-	})
+    async onSettled() {
+      await queryClient.invalidateQueries(['list-items'])
+    },
+  })
 }
 
 export {
-	useListItems,
-	useCreateListItem,
-	useUpdateListItem,
-	useRemoveListItem,
-	useListItem,
+  useListItems,
+  useCreateListItem,
+  useUpdateListItem,
+  useRemoveListItem,
+  useListItem,
 }
